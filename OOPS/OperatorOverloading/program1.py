b9ebeab3e13ccee021dd4480582e6ee53f340c12
Sritam Das <dsritam04@gmail.com>
2026-03-14 03:29:13 +0000

    Some changes

--- OOPS/OperatorOverloading/program1.py
+++ OOPS/OperatorOverloading/program1.py
@@ -1,6 +1,10 @@
 class java_book:
     def __init__(self,page):
         self.page=page
+        
+    def __add__(self, x):
+        print(self.page+x.page)
+
 class math_book:
     def __init__(self,page):
         self.page=page
