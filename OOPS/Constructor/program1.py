7623d585893e6015601a387458f330d46c7d9569
Sritam Das <dsritam04@gmail.com>
2026-03-14 03:02:26 +0000

    done changes

--- OOPS/Constructor/program1.py
+++ OOPS/Constructor/program1.py
@@ -1,8 +1,9 @@
 class a:
-    def __inti__(self):
+    def __init__(self):
         print("Class a")
         
 class b(a):
     def __init__(self):
+        super().__init__()
         print("Inside class B")
-        
+obj=b()
