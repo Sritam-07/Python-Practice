28b3450fae320fd07e27ac595d22582588f7b258
Sritam Das <dsritam04@gmail.com>
2026-03-22 17:10:39 +0000

    modify

--- Decorators/prog1.py
+++ Decorators/prog1.py
@@ -1,13 +1,16 @@
 def greet(fx):
-    def mfx():
+    def mfx(*args):
         print(f"Good morining Sir!")
-        fx()
+        fx(*args)
         print("Thanks for using this function")
     return mfx
 
-@greet
-def func():
-    print("Hello World")
+# @greet
+# def func():
+#     print("Hello World")
 
-func()
+@greet
+def func2(a,b):
+    print(a+b)
+func2(2,19)
        
